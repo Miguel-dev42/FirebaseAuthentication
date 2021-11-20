cacf439ce300aa81a4b5270c6790e34dd655972f
Miguel F <miguel.dev42@gmail.com>
2021-11-19 17:15:51 -0700

    Initial Commit

---
+++ FirebaseAuthenticationTests/FirebaseAuthenticationTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAuthenticationTestsApp.swift
+//  FirebaseAuthenticationTests
+//
+//  Created by Miguel Figueroa on 11/19/21.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAuthenticationTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
